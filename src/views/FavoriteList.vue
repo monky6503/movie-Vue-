a0829a371dc121a6598421a375f4cd95e626ed30
Chun-Ming, Liao <97944035+monky6503@users.noreply.github.com>
2024-07-17 00:49:14 +0800

    update

--- src/views/FavoriteList.vue
+++ src/views/FavoriteList.vue
@@ -77,12 +77,27 @@
         <figure class="py-7 md:px-5">
           <img :src="movie.Poster" alt="Movie" class="rounded-lg" />
         </figure>
-        <div class="flex flex-col justify-between p-3">
+        <div class="flex flex-col flex-1 justify-between p-3">
           <h2
             class="flex justify-center text-2xl text-center font-semibold py-2"
           >
             {{ movie.Title }}({{ movie.Year }})
           </h2>
+          <div class="ml-auto relative p-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+              class="w-14"
+            >
+              <path
+                d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+                fill="#EFD767"
+              />
+            </svg>
+            <span class="absolute left-[30px] top-[27px]">
+              {{ movie.imdbRating === 'N/A' ? '' : movie.imdbRating }}
+            </span>
+          </div>
           <div>
             <ul class="pl-10 list-disc">
               <li class="p-2 list-side">Released: {{ movie.Released }}</li>
@@ -100,7 +115,7 @@
             </ul>
           </div>
           <div class="flex items-center justify-between p-3">
-            <div class="flex items-center gap-2">
+            <div class="flex flex-wrap items-center gap-2">
               <div
                 v-for="(text, index) in movie.Genre.split(',')"
                 :key="index"
